63f31dc9fc733ff859a51a430ef5b1477718e9d4
Suraj <suraj@v-comply.com>
2022-06-08 19:13:59 +0530

    first commit

---
+++ .eleventy.js
@@ -0,0 +1,41 @@
+const sortByDisplayOrder = require('./src/utils/sort-by-display-order.js');
+const dateFilter = require('./src/filters/date-filter.js');
+const w3DateFilter = require('./src/filters/w3-date-filter.js');
+const rssPlugin = require('@11ty/eleventy-plugin-rss');
+module.exports = (config) => {
+    config.addFilter('dateFilter', dateFilter);
+    config.addFilter('w3DateFilter', w3DateFilter);
+    config.addPassthroughCopy('./src/images/');
+    config.addPlugin(rssPlugin);
+    config.setUseGitIgnore(false);
+    config.addCollection('work', collection => {
+        return sortByDisplayOrder(collection.getFilteredByGlob('./src/work/*.md'));
+      });
+      
+      // Returns work items, sorted by display order then filtered by featured
+      config.addCollection('featuredWork', collection => {
+        return sortByDisplayOrder(collection.getFilteredByGlob('./src/work/*.md')).filter(
+          x => x.data.featured
+        );
+      });
+      config.addCollection('blog', collection => {
+        return [...collection.getFilteredByGlob('./src/posts/*.md')].reverse();
+      });
+      config.addCollection('people', collection => {
+        return collection.getFilteredByGlob('./src/people/*.md').sort((a, b) => {
+          return Number(a.fileSlug) > Number(b.fileSlug) ? 1 : -1;
+        });
+      });
+    return {
+        markdownTemplateEngine: 'njk',
+        dataTemplateEngine: 'njk',
+        htmlTemplateEngine: 'njk',
+      dir: {
+        input: 'src',
+        output: 'dist'
+      }
+    };
+   
+  };
+
+
